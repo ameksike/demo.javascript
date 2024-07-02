34c97844f678400434aa57e09519b6b49e9a828d
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-07-02 20:41:17 +0200

    update imgs

--- react.state.context/src/components/IconHeader.tsx
+++ react.state.context/src/components/IconHeader.tsx
@@ -1,11 +1,11 @@
 import reactLogo from '../../assets/react.svg'
-// import viteLogo from '../vite.svg'
+import viteLogo from '../../assets/vite.svg'
 
 export function IconHeader({ title = "Vite + React + Context" }) {
   return (
     <div>
       <a href="https://vitejs.dev" target="_blank">
-        <img src={reactLogo} className="logo" alt="Vite logo" />
+        <img src={viteLogo} className="logo" alt="Vite logo" />
       </a>
       <a href="https://react.dev" target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
