22610010288dd5a3181448b060245880a92dbf91
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-07-03 08:32:47 +0200

    clean app

--- react.state.context/src/App.tsx
+++ react.state.context/src/App.tsx
@@ -1,10 +1,6 @@
 
 import './App.css'
-import CounterView from './components/CounterView'
-import CounterAct from './components/CounterAct'
 import { CounterProvider } from './services/CounterContext'
-import { IconHeader } from './components/IconHeader'
-
 import Menu from './components/Menu'
 
 
