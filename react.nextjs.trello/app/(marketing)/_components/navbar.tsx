bd3f155c34e670c6d45c87c8e600c7cf64819c1c
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-10-05 12:49:34 +0200

    Refactoring of  the authorization folder

--- react.nextjs.trello/app/(marketing)/_components/navbar.tsx
+++ react.nextjs.trello/app/(marketing)/_components/navbar.tsx
@@ -10,10 +10,10 @@ export function Navbar() {
 
                 <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
                     <Button size="sm" variant="outline" asChild >
-                        <Link href="/sign-in">Login</Link>
+                        <Link href="/auth/sign-in">Login</Link>
                     </Button>
                     <Button size="sm" asChild>
-                        <Link href="/sign-up">Get Taskify for free</Link>
+                        <Link href="/auth/sign-up">Get Taskify for free</Link>
                     </Button>
                 </div>
 
