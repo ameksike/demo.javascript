3284950c8e15ecbac80c1f623f0a7c1bbfadfaca
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-10-07 18:43:39 +0200

    add metadata to the title

--- react.nextjs.trello/app/(dashboard)/organization/[id]/layout.tsx
+++ react.nextjs.trello/app/(dashboard)/organization/[id]/layout.tsx
@@ -1,5 +1,13 @@
 import { LayoutParam } from "@/model/LayoutParam";
 import { OrgControl } from "../_components/control";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+    const { orgSlug } = auth();
+    return {
+        title: orgSlug || "organization"
+    }
+}
 
 export default function ({ children }: LayoutParam) {
     return (
